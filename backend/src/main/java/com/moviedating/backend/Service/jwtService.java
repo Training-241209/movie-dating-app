6f75aee6831253b471657af8b05af9ae095a7c35
Patrick Ferguson <pfergie2016@gmail.com>
2025-01-08 16:53:26 -0600

    fixed backend

--- backend/src/main/java/com/moviedating/backend/Service/jwtService.java
+++ backend/src/main/java/com/moviedating/backend/Service/jwtService.java
@@ -2,6 +2,7 @@ package com.moviedating.backend.Service;
 
 import com.moviedating.backend.Entity.Account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,6 @@ import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
-
     @Value("${jwt.secret}")
     private String secretKey;
 
