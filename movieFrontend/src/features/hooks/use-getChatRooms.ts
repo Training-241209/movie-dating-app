7dc72c31883be026672d0359add13fad768ac3ca
Justin Lui <jklui11032gmail.com>
2025-01-14 11:52:42 -0500

    fixed shown conversations

--- movieFrontend/src/features/hooks/use-getChatRooms.ts
+++ movieFrontend/src/features/hooks/use-getChatRooms.ts
@@ -1,13 +1,15 @@
 import { axiosInstance } from "@/lib/axios-config";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { ChatRoomUserSchema } from "../schemas/chatRoomUserSchema";
-
-export function useGetChatRooms(senderId: string) : UseQueryResult<ChatRoomUserSchema[]> {
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "./use-Auth";
+export function useGetChatRooms() {
+    const {data: auth} = useAuth()
     return useQuery({
         queryKey: ['chatRooms'],
-        queryFn: async () => {
-            const resp = await axiosInstance.get(`/chatRoom/${senderId}`);
-            return resp.data;
+        queryFn: async() =>{
+              const response = await axiosInstance.get(`http://localhost:8080/api/match/${auth?.username}`)
+              console.log("DATA ",response.data)
+              return response.data;
+
         }
-    });
+    })
 }
--- movieFrontend/src/routes/_chat/chat.$senderId.$recipientId.route.tsx
+++ movieFrontend/src/routes/_chat/chat.$senderId.$recipientId.route.tsx
@@ -5,14 +5,21 @@ import {
   ChatBoxContents,
 } from '@/components/shared/chat'
 import { Message } from '@/components/shared/message'
+import { useAuth } from '@/features/hooks/use-Auth'
 export const Route = createFileRoute('/_chat/chat/$senderId/$recipientId')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
   //Call get messages from websocket
+  const { senderId, recipientId } = Route.useParams()
+  const {data:auth } = useAuth()
   return (
+
     <div>
+      {senderId}
+      {recipientId}
+      {auth?.username}
       <ChatBoxCentering>
         <ChatBoxCard>
           <ChatBoxContents>
